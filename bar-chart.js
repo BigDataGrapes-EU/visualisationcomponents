129df13f0170bf85eb226d3e33d247cf6809b8d0
Francisco G <fsalvador23@gmail.com>
2018-08-24 14:15:06 -0500

    Added components to be implemented

--- bar-chart.js
+++ bar-chart.js
@@ -1,30 +1,45 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import Chart from 'chart.js'
 
 /**
- * `bar-chart`
- * A barchart visualisation
- *
- * @customElement
- * @polymer
- * @demo demo/index.html
- */
+* `bar-chart`
+* A barchart visualisation
+*
+* @customElement
+* @polymer
+* @demo demo/index.html
+*/
 class BarChart extends PolymerElement {
   static get template() {
     return html`
-      <style>
-        :host {
-          display: block;
-        }
-      </style>
-      <h2>Hello [[prop1]]!</h2>
+    <style>
+      :host {
+        display: block;
+      }
+    </style>
+    Width: [[width]]!
+    Height: [[height]]!
+    <div>
+    </div>
     `;
   }
   static get properties() {
     return {
-      prop1: {
-        type: String,
-        value: 'bar-chart',
+      height: {
+        type: Number,
+        value: '200',
       },
+      width: {
+        type: Number,
+        value: '400',
+      },
+      data: {
+        type: Array,
+        value: function() {
+          return [];
+        },
+        //observer: '_dataChanged'
+      }
     };
   }
 }
